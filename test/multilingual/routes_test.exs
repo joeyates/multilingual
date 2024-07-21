6b6c8b18ed486dd568f57d7150af7b13839d5329
Joe Yates <joe.g.yates@gmail.com>
2024-07-21 16:58:06 +0200

    Get the locale for a given path

--- test/multilingual/routes_test.exs
+++ test/multilingual/routes_test.exs
@@ -1,4 +1,31 @@
+defmodule Multilingual.RoutesTest.PageController do
+  use Phoenix.Controller
+end
+
+defmodule Multilingual.RoutesTest.Router do
+  use Phoenix.Router
+
+  import Multilingual.Routes, only: [metadata: 1]
+
+  scope "/", Multilingual.RoutesTest do
+    get "/about", PageController, :about, metadata("en")
+  end
+end
+
 defmodule Multilingual.RoutesTest do
   use ExUnit.Case
   doctest Multilingual.Routes
+
+  import Multilingual.Routes
+  alias Multilingual.RoutesTest.Router
+
+  describe "path_locale/2" do
+    test "returns a path's locale" do
+      assert path_locale(Router, "/about") == "en"
+    end
+
+    test "returns nil when the path is not found" do
+      assert path_locale(Router, "/doesnt_exist") == nil
+    end
+  end
 end
