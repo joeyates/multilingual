2448498aa9fc41aee4dffd260d55d1382076430a
Joe Yates <joe.g.yates@gmail.com>
2024-07-22 11:30:07 +0200

    Fetch localized paths

--- lib/multilingual/routes.ex
+++ lib/multilingual/routes.ex
@@ -1,4 +1,55 @@
 defmodule Multilingual.Routes do
+  def localized_path(router, path, locale) do
+    case localized_route(router, path, locale) do
+      nil ->
+        nil
+
+      route ->
+        route.path
+    end
+  end
+
+  defp localized_route(router, path, locale) do
+    with {:ok, route} <- find_path_route(router, path),
+         {:ok, localized} <- find_localized_route(router, route, locale) do
+      localized
+    else
+      _any ->
+        nil
+    end
+  end
+
+  defp find_localized_route(router, route, locale) do
+    other =
+      Phoenix.Router.routes(router)
+      |> Enum.find(fn other ->
+        case locale(other) do
+          {:ok, ^locale} ->
+            same_view(route, other)
+
+          _any ->
+            false
+        end
+      end)
+
+    case other do
+      nil -> {:error, :not_found}
+      other -> {:ok, other}
+    end
+  end
+
+  defp same_view(route_1, route_2) do
+    with true <- route_1.verb == route_2.verb,
+         true <- route_1.plug == route_2.plug,
+         true <- route_1.plug_opts == route_2.plug_opts,
+         true <- route_1.helper == route_2.helper do
+      true
+    else
+      _any ->
+        false
+    end
+  end
+
   @doc """
   Creates metadata for multilingual routes.
 
--- test/multilingual/routes_test.exs
+++ test/multilingual/routes_test.exs
@@ -5,6 +5,24 @@ defmodule Multilingual.RoutesTest do
   import Multilingual.Routes
   alias Multilingual.Test.Project.Router
 
+  describe "localized_path/3" do
+    test "returns the localized path" do
+      assert localized_path(Router, "/about", "it") == "/it/chi-siamo"
+    end
+
+    test "returns an error tuple when the path doesn't exist" do
+      assert localized_path(Router, "/doesnt_exist", "it") == nil
+    end
+
+    test "returns an error tuple when the path is not localized" do
+      assert localized_path(Router, "/monolingual", "it") == nil
+    end
+
+    test "returns an error tuple when the path is not localized for the requested locale" do
+      assert localized_path(Router, "/about", "fr") == nil
+    end
+  end
+
   describe "path_locale/2" do
     test "returns a path's locale" do
       assert path_locale(Router, "/about") == "en"
