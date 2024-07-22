7497e2e115473afc93ba4921f3d938bc2b2e325f
Joe Yates <joe.g.yates@gmail.com>
2024-07-22 22:04:20 +0200

    More indentation fixes

--- lib/multilingual/view.ex
+++ lib/multilingual/view.ex
@@ -36,15 +36,15 @@ defmodule Multilingual.View do
 
   ## Examples
 
-    iex> view = %Multilingual.View{locale: "en", path: "/about"}
-    ...> conn = Plug.Conn.put_private(%Plug.Conn{}, :multilingual, view)
-    ...> Multilingual.View.fetch_key(conn, :path)
-    "/about"
+      iex> view = %Multilingual.View{locale: "en", path: "/about"}
+      ...> conn = Plug.Conn.put_private(%Plug.Conn{}, :multilingual, view)
+      ...> Multilingual.View.fetch_key(conn, :path)
+      "/about"
 
-    iex> view = %Multilingual.View{locale: "en", path: "/about"}
-    ...> conn = Plug.Conn.put_private(%Plug.Conn{}, :multilingual, view)
-    ...> Multilingual.View.fetch_key(conn, :bad_key)
-    ** (FunctionClauseError) no function clause matching in Multilingual.View.fetch_key/2
+      iex> view = %Multilingual.View{locale: "en", path: "/about"}
+      ...> conn = Plug.Conn.put_private(%Plug.Conn{}, :multilingual, view)
+      ...> Multilingual.View.fetch_key(conn, :bad_key)
+      ** (FunctionClauseError) no function clause matching in Multilingual.View.fetch_key/2
   """
   def fetch_key(%Plug.Conn{} = conn, key) when key in @attrs do
     Map.fetch!(conn.private.multilingual, key)
