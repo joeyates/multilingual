2103155c6d1a7de1a003c39ab3671100bc8cd2d4
Joe Yates <joe.g.yates@gmail.com>
2024-07-22 21:00:55 +0200

    Prepare release

--- mix.exs
+++ mix.exs
@@ -1,14 +1,19 @@
 defmodule Multilingual.MixProject do
   use Mix.Project
 
+  @app :multilingual
+
   def project do
     [
-      app: :multilingual,
+      app: @app,
       version: "0.1.0",
       elixir: "~> 1.17",
-      start_permanent: Mix.env() == :prod,
+      description:
+        "Simplify handling localized routes in Elixir Phoenix applications, with and without LiveView",
       deps: deps(),
-      elixirc_paths: elixirc_paths(Mix.env())
+      elixirc_paths: elixirc_paths(Mix.env()),
+      package: package(),
+      start_permanent: Mix.env() == :prod
     ]
   end
 
@@ -18,15 +23,26 @@ defmodule Multilingual.MixProject do
     ]
   end
 
-  defp elixirc_paths(:test), do: ["lib", "test/project"]
-  defp elixirc_paths(_env), do: ["lib"]
-
   defp deps do
     [
       {:ex_cldr, ">= 2.39.2"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:jason, ">= 1.4.1"},
       {:phoenix, ">= 1.7.10"},
       {:phoenix_live_view, ">= 0.20.1", optional: true}
     ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/project"]
+  defp elixirc_paths(_env), do: ["lib"]
+
+  defp package do
+    %{
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/joeyates/multilingual"
+      },
+      maintainers: ["Joe Yates"]
+    }
   end
 end
