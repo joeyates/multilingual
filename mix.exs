b96f743afff4a60696d8fb6f37afa4a3226951cb
Joe Yates <joe.g.yates@gmail.com>
2024-07-26 08:59:30 +0200

    Correct typo

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Multilingual.MixProject do
   def project do
     [
       app: @app,
-      version: "0.1.7",
+      version: "0.1.8",
       elixir: "~> 1.17",
       description:
         "Simplify handling localized routes in Elixir Phoenix applications, with and without LiveView",
